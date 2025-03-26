c6f04e37b53db326ca12ca94ebba4278a729c8ba
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-03-26 22:15:57 +0100

    edit first model and experiment with the input data

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(default="")
+    newsletter = models.BooleanField(default=False)
+    account_balance = models.FloatField(blank=True, null=True)
